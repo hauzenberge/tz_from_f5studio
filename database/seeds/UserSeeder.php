81609017b55c1dfa0c1e80e29b9617bcb94c8b5d
Alex <62619586+hauzenberge@users.noreply.github.com>
2021-04-20 19:02:19 +0300

    Update UserSeeder.php

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -18,6 +18,15 @@ class UserSeeder extends Seeder
         	'role_id' => 1, 
         	'email' => 'admin@admin.com',
         	'password' => bcrypt('password'),
-    ]);
+        ]);
+        
+        App\User::create([
+        	'first_name' => 'Client', 
+        	'last_name' => 'Adminskiy', 
+        	'phone' => '+380682092340', 
+        	'role_id' => 2, 
+        	'email' => 'client@admin.com',
+        	'password' => bcrypt('password'),
+        ]);
     }
 }
